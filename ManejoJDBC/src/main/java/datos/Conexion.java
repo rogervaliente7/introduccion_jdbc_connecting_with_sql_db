d3a3ff837b7eaaced38792b1dbc200e47c096110
roger <rogervalriveraj@gmail.com>
2024-06-14 17:49:48 -0600

    Adding pool connection

--- ManejoJDBC/src/main/java/datos/Conexion.java
+++ ManejoJDBC/src/main/java/datos/Conexion.java
@@ -1,38 +1,51 @@
 package datos;
 
 import java.sql.*;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
 
 public class Conexion {
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/test_database";
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASS = "root_test";
-    
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+    private static BasicDataSource dataSource;
+
+    public static DataSource getDataSource() {
+        if (dataSource == null) {
+            dataSource = new BasicDataSource();
+            dataSource.setUrl(JDBC_URL);
+            dataSource.setUsername(JDBC_USER);
+            dataSource.setPassword(JDBC_PASS);
+            dataSource.setInitialSize(50);
+        }
+        return dataSource;
     }
-    
-    public static void close(ResultSet rs){
+
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+
+    public static void close(ResultSet rs) {
         try {
             rs.close();
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
         }
     }
-    
-    public static void close(PreparedStatement stmt){
+
+    public static void close(PreparedStatement stmt) {
         try {
             stmt.close();
         } catch (SQLException ex) {
             ex.printStackTrace(System.out);
         }
     }
-    
-    public static void close(Connection conn){
+
+    public static void close(Connection conn) {
         try {
             conn.close();
         } catch (SQLException ex) {
-           ex.printStackTrace(System.out);
+            ex.printStackTrace(System.out);
         }
     }
-    
 }
